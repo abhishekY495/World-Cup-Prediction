162e047763020988e8a3434df4ec01d42510c039
Abhishek <67749693+abhishekY495@users.noreply.github.com>
2022-12-12 14:27:21 +0530

    Fixed colorNames

--- JS/enableForwardButtons.js
+++ JS/enableForwardButtons.js
@@ -15,49 +15,49 @@ semiGroup2Country2ScoreInput.addEventListener("input", () => { forwardColor() })
 finalCountry1ScoreInput.addEventListener("input", () => { forwardColor() });
 finalCountry2ScoreInput.addEventListener("input", () => { forwardColor() });
 
-const dull = "#02be02";
-const bright = "#245c1e";
+const brightColor = "#02be02";
+const dullColor = "#245c1e";
 function forwardColor() {
   if (!nedGoalsInput.value && !argGoalsInput.value) {
-    forwardQuarterGroup1.style.backgroundColor = bright;
+    forwardQuarterGroup1.style.backgroundColor = dullColor;
   } else {
-    forwardQuarterGroup1.style.backgroundColor = dull;
+    forwardQuarterGroup1.style.backgroundColor = brightColor;
   }
   if (!croGoalsInput.value && !braGoalsInput.value) {
-    forwardQuarterGroup2.style.backgroundColor = bright;
+    forwardQuarterGroup2.style.backgroundColor = dullColor;
   } else {
-    forwardQuarterGroup2.style.backgroundColor = dull;
+    forwardQuarterGroup2.style.backgroundColor = brightColor;
   }
   if (!engGoalsInput.value && !fraGoalsInput.value) {
-    forwardQuarterGroup3.style.backgroundColor = bright;
+    forwardQuarterGroup3.style.backgroundColor = dullColor;
   } else {
-    forwardQuarterGroup3.style.backgroundColor = dull;
+    forwardQuarterGroup3.style.backgroundColor = brightColor;
   }
   if (!marGoalsInput.value && !porGoalsInput.value) {
-    forwardQuarterGroup4.style.backgroundColor = bright;
+    forwardQuarterGroup4.style.backgroundColor = dullColor;
   } else {
-    forwardQuarterGroup4.style.backgroundColor = dull;
+    forwardQuarterGroup4.style.backgroundColor = brightColor;
   }
   //
   if (
     !semiGroup1Country1ScoreInput.value && !semiGroup1Country2ScoreInput.value
   ) {
-    forwardSemiGroup1.style.backgroundColor = bright;
+    forwardSemiGroup1.style.backgroundColor = dullColor;
   } else {
-    forwardSemiGroup1.style.backgroundColor = dull;
+    forwardSemiGroup1.style.backgroundColor = brightColor;
   }
   if (
     !semiGroup2Country1ScoreInput.value && !semiGroup2Country2ScoreInput.value
   ) {
-    forwardSemiGroup2.style.backgroundColor = bright;
+    forwardSemiGroup2.style.backgroundColor = dullColor;
   } else {
-    forwardSemiGroup2.style.backgroundColor = dull;
+    forwardSemiGroup2.style.backgroundColor = brightColor;
   }
   //
   if (!finalCountry1ScoreInput.value && !finalCountry2ScoreInput.value) {
-    forwardFinal.style.backgroundColor = bright;
+    forwardFinal.style.backgroundColor = dullColor;
   } else {
-    forwardFinal.style.backgroundColor = dull;
+    forwardFinal.style.backgroundColor = brightColor;
   }
 }
 
